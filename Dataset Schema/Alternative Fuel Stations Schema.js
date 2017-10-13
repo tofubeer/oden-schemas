9ddf7a1af57ced8b6c8c9dae269b02d58cda3441
Cody Clattenburg <c.clattenburg@shaw.ca>
2017-10-13 16:41:34 -0700

    formatting

--- Dataset Schema/Alternative Fuel Stations Schema.js
+++ Dataset Schema/Alternative Fuel Stations Schema.js
@@ -1,42 +1,42 @@
 /*This file was created for Terratap-Technologies-Inc by Cody Clattenburg, Sam Collins, Martin Suryadi, and Sergio Josue Villegas. This file is under the protection of the Apache 2.0 License.*/
 
 {
-  "type": "Feature",
-  "geometry": {
-    "coordinates": {
-      "items": {
-        "type": "float",
+	"type": "Feature",
+	"geometry": {
+		"coordinates": {
+			"items": {
+				"type": "float",
 				"example": 49.222
-      },
-      "type": "array",
+			},
+			"type": "array",
 			"example": [49.222, -122.222]
-    },
-    "type": {
-      "type": "String",
+		},
+		"type": {
+			"type": "String",
 			"example": "Point",
 			"description": "This indicates that the station is geologically a Point."
-    }
-  },
-  "properties": {
-    "fuelType": {
-      "type": "string",
+		}
+	},
+	"properties": {
+		"fuelType": {
+			"type": "string",
 			"example": "Hydrogen"
-    },
-    "name": {
-      "type": "string",
+		},
+		"name": {
+			"type": "string",
 			"example": "Bob's Charging Station",
 			"default": "Name Unavailible"
-    },
-    "address": {
-      "type": "string",
+		},
+		"address": {
+			"type": "string",
 			"example": "123 West Rd",
 			"default": "Address Unavailible"
-    },
-	  "access": {
-	    "type": "string",
+		},
+		"access": {
+			"type": "string",
 			"example": "City Vehicle use only",
 			"default": "Public"
-	  }
-  },
+		}
+	},
 }
 "type": "object"
